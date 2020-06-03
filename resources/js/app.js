3060e7df5472a5ec1ab9ff59ae32dff7a484d9cd
Usman <Usmanqadeer085@gmail.com>
2020-06-03 22:46:03 +0500

    Student

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 require('./bootstrap');
+import Vue from 'vue';
+import vuetify from './vuetify';
+import router from './router';
+
+//
+
+
+import Main from './components/MainComponent';
+
+
+new Vue({
+    el: '#app',
+    vuetify,
+    router,
+    components: {
+        'main-component': Main
+    }
+
+});
---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+
+<login></login>
+
+</template>
+
+
+<script>
+import login from './StudentComponents/LoginComponent/LoginComponent';
+
+    export default {
+         components:{login},
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
